36c10cd4a93b96ff991645183253764ba711d918
Yure Pablo <48031838+yp1plus@users.noreply.github.com>
2019-08-08 16:41:54 -0300

    Atualização da Função strsubst

--- substDeStr.c
+++ substDeStr.c
@@ -4,8 +4,9 @@
 
 void strsubst(char *s1, char * s2, char * s3){
     int i, flag = 0, j = 0;
+    int size_str1 = strlen(s1), size_str2 = strlen(s2); //evita chamar a funcao a todo o momento
     //printf("%ld\n", strlen(s1));
-    for(i = 0; i < strlen(s1); i++){
+    for(i = 0; i < size_str1; i++){
         if(s1[i] == s2[flag]){
             //printf("s1[%d] == %c, s2[%d] == %c\n", i, s1[i], flag, s2[flag]);
             flag++;
@@ -17,7 +18,7 @@ void strsubst(char *s1, char * s2, char * s3){
             //printf("j = %d\n", j);
             j = i;
         }
-        if(flag == strlen(s2)){
+        if(i == size_str1 - 1 && flag == size_str2){ //evita erros do tipo "ida" tmb permitir a substituição
             //printf("%s\n", s1);
             strcpy(s1 + j, s3);
         }
@@ -29,7 +30,7 @@ int main(){
     //printf("\"\"");
 
     scanf("%s %s %s", s1, s2, s3);
-    if(!strcmp(s3, "\"\"")){
+    if(!strcmp(s3, "\"\"")){ //??????
         strcpy(s3, "");
     }
     strsubst(s1, s2, s3);
