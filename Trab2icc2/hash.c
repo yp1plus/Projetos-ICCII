0bfac96dc2d5f9bb5f2d55cf5bc5e4b65d4f6f30
Yure Pablo <48031838+yp1plus@users.noreply.github.com>
2019-11-24 00:53:14 -0300

    versão comentada

--- Trab2icc2/hash.c
+++ Trab2icc2/hash.c
@@ -1,113 +1,112 @@
 #include "hash.h"
-// TODAS FUNCOES USANDO "###" COMO STRING AINDA INEXISTENTE E "####" COMO STRING SUBSTITUTA (REMOVIDA)
-// EX: tablelas todas inicializadas com "###"
-// EX: na remocao, substituir palavra removida (e sua traducao) por "####"
 
+/*! Aloca espaço na heap para uma palavra de, no máximo, 512 caracteres e retorna */
 char* word_alloc(void){
-    char* word=NULL;
+    char* word = NULL;
 
-    word=(char*)malloc(sizeof(char)*TAMANHO_DA_PALAVRA+1);
-    if(word==NULL){printf("Insuficient space in memory.\n");exit (1);}
+    word = (char*) malloc(sizeof(char)*(WORD_SIZE + ZERO_BAR));
+    
+    if(word == NULL){
+        printf("Insuficient space in memory.\n");
+        exit(1);
+    }
+    
     return word;
 }
 
+/*! Aloca espaço na heap para uma tabela hash com, no máximo, 10 mil palavras e retorna */
 char** table_alloc(void){
-    int i;
-    char** table=NULL;
+    char** table = NULL;
+    int i = 0;
 
-    table=(char**)malloc(sizeof(char*)*TAMANHO_DA_ENTRADA);
-    if(table==NULL){printf("Insuficient space in memory.\n");exit (1);}
-
-    for(i=0;i<TAMANHO_DA_ENTRADA;i++){
-        table[i]=(char*)malloc(sizeof(char)*(TAMANHO_DA_PALAVRA+1)); //+1 para incluir o \0
-        if(table[i]==NULL){printf("Insuficient space in memory.\n");exit (1);}
-        strcpy(table[i],ENTRADA_VAZIA);
+    table = (char**) malloc(sizeof(char*)*INPUT_SIZE);
+    
+    if(table == NULL){
+        printf("Insuficient space in memory.\n");
+        exit (1);
     }
-return table;
+
+    for(i = 0; i < INPUT_SIZE; i++){
+        table[i] = word_alloc();
+
+        strcpy(table[i], EMPTY_INPUT); /**< inicializa cada palavra com indicador de vazio */
+    }
+    
+    return table;
 }
 
-int stringSum(char *key)
-{
-    // retorna um inteiro, soma dos caracteres da string
+/*! Retorna um inteiro, a soma dos caracteres em ASCII da string key */
+int stringSum(char *key){
     int sum = 0;
     int i = 0;
+    
     while(key[i] != '\0'){
-        sum += key[i];
-        i++;
+        sum += key[i++];
     }
+    
     return sum;
 }
 
-int hash_code(char **table, char *key)
-{
-    int guess = stringSum(key) % TAMANHO_DA_ENTRADA;
-    // realiza o procedimento de progressive overflow, olhando a proxima posicao
+/*! Retorna um inteiro, o codigo hash da string key na tabela hash, com overflow progressivo */
+int hash_code(char **table, char *key){
+    int guess = stringSum(key) % INPUT_SIZE; /**< função hash */
+
     while(1){
-        guess = (guess+1)%TAMANHO_DA_ENTRADA;
-        if(!strcmp(table[guess], ENTRADA_VAZIA) || !strcmp(table[guess], ENTRADA_REMOVIDA) || !strcmp(table[guess], key)){
+        if(!strcmp(table[guess], EMPTY_INPUT) || !strcmp(table[guess], INPUT_REMOVED) || !strcmp(table[guess], key)){
             return guess;
         }
+
+        guess = (guess + 1) % INPUT_SIZE;
     }
 }
 
-int find_ht(char **table, char *key)
-{
-    // conferindo se achou de primeira
-    int guess = stringSum(key) % TAMANHO_DA_ENTRADA;
-    if(!strcmp(table[guess], key)){
-        return guess;
-    }
-    // caso nao acertou a primeira, vai conferindo proximas ate percorrer todas 10000 possiveis entradas ou
-    // ate encontrar uma entrada virgem (ENTRADA_VAZIA)
+/*! Retorna um inteiro, a posição da string key na tabela hash, ou um endereço inválido, caso ela não exista */
+int find_ht(char **table, char *key){
+    int code = stringSum(key);
+    int guess;
     int i = 0;
-    for(i = 0; i < TAMANHO_DA_ENTRADA; i++){
-        guess = (guess+1)%TAMANHO_DA_ENTRADA;
-        // caso encontrar
+
+    /**
+     * Percorre os indices da tabela até encontrar a chave ou um campo vazio.
+     * No pior caso, percorre todo o tamanho da entrada.
+     */
+    for(i = 0; i < INPUT_SIZE; i++){
+        guess = (code + i) % INPUT_SIZE;
+    
         if(!strcmp(table[guess], key)){
             return guess;
         }
-        // caso nao encontrar
-        else if(!strcmp(table[guess], ENTRADA_VAZIA)){
-            return HASH_INVALIDO;
-        }
-        else{
-            continue;
+        
+        else if(!strcmp(table[guess], EMPTY_INPUT)){
+            return INVALID_HASH;
         }
     }
-    return HASH_INVALIDO;
+    
+    return INVALID_HASH;
 }
 
+/*! Insere a palavra original e a traduzida nas suas respectivas tabelas */
 void hash_insert(char **originalTable, char** translatedTable, char* originalWord, char* translatedWord){
-    int index;
+    int index = hash_code(translatedTable, translatedWord); /**< o codigo é gerado a partir da palavra traduzida */
 
-    //o endereço é dado a partir da palavra no idioma estrangeiro
-    index=hash_code(translatedTable,translatedWord);
-
-    strcpy(originalTable[index],originalWord);
-    strcpy(translatedTable[index],translatedWord);
+    strcpy(originalTable[index], originalWord);
+    strcpy(translatedTable[index], translatedWord);
 
     return;
 }
 
-void hash_remove(char** originalTable,char** translatedTable,char* translatedWord){
-    int guess,i;
-
-    guess=stringSum(translatedWord) % TAMANHO_DA_ENTRADA;
-
-    if(!strcmp(translatedTable[guess],translatedWord)){
-        strcpy(translatedTable[guess],ENTRADA_REMOVIDA);
-        strcpy(originalTable[guess],ENTRADA_REMOVIDA);
-        return;
+//!  Remove a palavra original e a traduzida das suas respectivas tabelas. 
+/*!
+  Note que, diferente da inserção, na remoção só é necessária a palavra traduzida, 
+  uma vez que o dado já existe e o indice é o mesmo.
+*/
+void hash_remove(char** originalTable, char** translatedTable, char* translatedWord){
+    int index = find_ht(translatedTable, translatedWord);
+    
+    if(index != INVALID_HASH){
+        strcpy(translatedTable[index], INPUT_REMOVED);
+        strcpy(originalTable[index], INPUT_REMOVED);
     }
 
-    for(i=0;i<TAMANHO_DA_ENTRADA;i++){
-        guess = (guess+1)%TAMANHO_DA_ENTRADA;
-
-        if(!strcmp(translatedTable[guess],translatedWord)){
-            strcpy(translatedTable[guess],ENTRADA_REMOVIDA);
-            strcpy(originalTable[guess],ENTRADA_REMOVIDA);
-            return;
-        }
-    }
     return;
 }
