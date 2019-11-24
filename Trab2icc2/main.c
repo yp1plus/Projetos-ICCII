9be92eba9056d8e58bd62f653cc60e9d13ea3005
Yure Pablo <48031838+yp1plus@users.noreply.github.com>
2019-11-24 00:54:38 -0300

    versão comentada

--- Trab2icc2/main.c
+++ Trab2icc2/main.c
@@ -1,68 +1,42 @@
 #include "hash.h"
 
+#define INITIAL_CHARACTERS 4
+
 int main(void){
+    char selector; /**< operador de inserção, de remoção ou de busca */
+    int index; /**< posição da chave na tabela hash ou endereço invalido */
+    char input[INITIAL_CHARACTERS]; /**< entrada considerando operador, ':' e espaços em branco */
 
+    char* originalWord = word_alloc();
+    char* translatedWord = word_alloc();
 
-    char selector;
-    
-    int index;
+    char** originalTable = table_alloc();
+    char** translatedTable = table_alloc();
 
-    char input[4];
-    char* originalWord=NULL;
-    char* translatedWord=NULL;
+    while(scanf("%s :", input) != EOF){
+        selector = input[0];
 
-    char** originalTable=NULL;
-    char** translatedTable=NULL;
-
-    originalTable=table_alloc();
-    translatedTable=table_alloc();
-
-    originalWord=word_alloc();
-    translatedWord=word_alloc();
-
-    while(scanf("%s :",input) != EOF){
-        selector=input[0];
-
-        switch (selector)
-        {
-        case 'A':
-            scanf("%s",originalWord);
-            scanf("%s",translatedWord);
-            hash_insert(originalTable,translatedTable,originalWord,translatedWord);
-            break;
-        
-        case 'R':
-            scanf("%s",translatedWord);
-            hash_remove(originalTable,translatedTable,translatedWord);
-            break;
-
-        case 'F':
-            scanf("%s",translatedWord);
-            index=find_ht(translatedTable,translatedWord);
-            if(index==HASH_INVALIDO) printf("hein?\n");
-            else printf("%s\n",originalTable[index]);
-            break;
-        
-        case 'a':
-            scanf("%s",originalWord);
-            scanf("%s",translatedWord);
-            hash_insert(originalTable,translatedTable,originalWord,translatedWord);
-            break;
-        
-        case 'r':
-            scanf("%s",translatedWord);
-            hash_remove(originalTable,translatedTable,translatedWord);
-            break;
-
-        case 'f':
-            scanf("%s",translatedWord);
-            index=find_ht(translatedTable,translatedWord);
-            if(index==HASH_INVALIDO) printf("hein?\n");
-            else printf("%s\n",originalTable[index]);
-            break;    
-
-        default:
-            break;
+        switch (selector){
+            case 'A': case 'a': /**< operação de inserção */
+                scanf("%s", originalWord);
+                scanf("%s", translatedWord);
+                hash_insert(originalTable, translatedTable, originalWord, translatedWord);
+                break;
+            case 'R': case 'r': /**< operação de remoção */
+                scanf("%s", translatedWord);
+                hash_remove(originalTable, translatedTable, translatedWord);
+                break;
+            case 'F': case 'f': /**< operação de busca */
+                scanf("%s", translatedWord);
+                
+                index = find_ht(translatedTable, translatedWord);
+                
+                if(index == INVALID_HASH) 
+                    printf("hein?\n");
+                else 
+                    printf("%s\n", originalTable[index]);
+                
+                break;
         }
     }
 
